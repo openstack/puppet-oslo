919d8fa329a813d1e76edeeee703644a2f28d2f3
Xingchao Yu <xingchao@unitedstack.com>
2016-01-22 01:42:34 +0800

    Clean up unused rspec test
    These rspec class tests are automatically generated
    by cookiecutter, but actually they are not used in puppet-oslo.

---
+++ spec/classes/oslo_init_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'oslo' do
+
+  shared_examples 'oslo' do
+
+    context 'with default parameters' do
+     it 'contains the logging class' do
+       is_expected.to contain_class('oslo::params')
+     end
+  end
+
+end
+end
