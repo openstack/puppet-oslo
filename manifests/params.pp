6ec80a2331414540cc5a9cbacd8d6d2c81171c85
ZhongShengping <chdzsp@163.com>
2016-11-08 11:23:26 +0800

    Fix common options position in params.pp
    These parameters are common for both systems,
    they could be moved out from 'case'.
    
    Change-Id: I71bdccfec45ecacc28491309be5d3ecd628d9278

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,21 +1,22 @@
+# ==Class: oslo::params
+#
 # Parameters for puppet-oslo
 #
 class oslo::params {
   include ::openstacklib::defaults
 
+  $pymongo_package_name = 'python-pymongo'
+  $pylibmc_package_name = 'python-pylibmc'
+
   case $::osfamily {
     'RedHat': {
       $sqlite_package_name          = undef
       $pymysql_package_name         = undef
-      $pymongo_package_name         = 'python-pymongo'
-      $pylibmc_package_name         = 'python-pylibmc'
       $python_memcache_package_name = 'python-memcached'
     }
     'Debian': {
       $sqlite_package_name          = 'python-pysqlite2'
       $pymysql_package_name         = 'python-pymysql'
-      $pymongo_package_name         = 'python-pymongo'
-      $pylibmc_package_name         = 'python-pylibmc'
       $python_memcache_package_name = 'python-memcache'
     }
     default: {
