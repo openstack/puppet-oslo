6eafe3052adba73ab26939146a65fb864b245aec
Tobias Urdin <tobias.urdin@binero.se>
2018-11-24 15:38:11 +0100

    Convert to rspec-puppet-facts
    Change-Id: I87cae085eb3ae0967df65522189967367f8eb72c

--- spec/classes/oslo_init_spec.rb
+++ spec/classes/oslo_init_spec.rb
@@ -1,15 +1,21 @@
 require 'spec_helper'
 
 describe 'oslo' do
-
   shared_examples 'oslo' do
-
     context 'with default parameters' do
-     it 'contains the logging class' do
-       is_expected.to contain_class('oslo::params')
-     end
+      it { should contain_class('oslo::params') }
     end
-
   end
 
+  on_supported_os({
+    :supported_os => OSDefaults.get_supported_os
+  }).each do |os,facts|
+    context "on #{os}" do
+      let (:facts) do
+        facts.merge!(OSDefaults.get_facts())
+      end
+
+      it_behaves_like 'oslo'
+    end
+  end
 end
