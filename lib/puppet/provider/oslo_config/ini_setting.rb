6cd2bc408d55c7bdaf43013e1a2c0a1c4fd1fa9e
Takashi Kajinami <tkajinam@redhat.com>
2020-05-04 14:39:35 +0900

    Deprecate oslo_config resource
    ... because /etc/oslo/oslo.conf has never been actually used.
    
    Change-Id: Ib327d97b13877b5ce2651e5a4f15b9d11c939000

--- lib/puppet/provider/oslo_config/ini_setting.rb
+++ lib/puppet/provider/oslo_config/ini_setting.rb
@@ -3,6 +3,11 @@ Puppet::Type.type(:oslo_config).provide(
   :parent => Puppet::Type.type(:openstack_config).provider(:ini_setting)
 ) do
 
+  def create
+    super
+    warning('oslo_config is deprecated, and will be removed in a future release')
+  end
+
   def self.file_path
     '/etc/oslo/oslo.conf'
   end
