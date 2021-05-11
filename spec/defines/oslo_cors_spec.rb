83e2c42821c9f1f7cba052355b90b38b97198f95
Takashi Kajinami <tkajinam@redhat.com>
2021-05-12 00:00:25 +0900

    Cors: Accept array for ListOpts
    The following four parameters of cors middleware are ListOpt, thus
    accepting array for these options allows users to define parameters
    in a similar format.
    
     - allowed_origin
     - expose_headers
     - allow_methods
     - allow_headers
    
    Change-Id: I4478d209c23e7468e6f36fe417acc01d7b986938
    (cherry picked from commit 769ff6dec9cbc9fe1c48b6493027407b37217f96)

--- spec/defines/oslo_cors_spec.rb
+++ spec/defines/oslo_cors_spec.rb
@@ -15,7 +15,46 @@ describe 'oslo::cors' do
        is_expected.to contain_keystone_config('cors/allow_methods').with_value('<SERVICE DEFAULT>')
        is_expected.to contain_keystone_config('cors/allow_headers').with_value('<SERVICE DEFAULT>')
       end
+    end
 
+    context 'with parameters' do
+      let (:params) do
+        {
+          :allowed_origin    => 'https://horizon.example.com,https://horizon.example2.com',
+          :allow_credentials => true,
+          :expose_headers    => 'HeaderOne,HeaderTwo',
+          :max_age           => 3600,
+          :allow_methods     => 'GET,HEAD',
+          :allow_headers     => 'HeaderThree,HeaderFour',
+        }
+      end
+
+      it 'configure cors params' do
+       is_expected.to contain_keystone_config('cors/allowed_origin').with_value('https://horizon.example.com,https://horizon.example2.com')
+       is_expected.to contain_keystone_config('cors/allow_credentials').with_value(true)
+       is_expected.to contain_keystone_config('cors/expose_headers').with_value('HeaderOne,HeaderTwo')
+       is_expected.to contain_keystone_config('cors/max_age').with_value(3600)
+       is_expected.to contain_keystone_config('cors/allow_methods').with_value('GET,HEAD')
+       is_expected.to contain_keystone_config('cors/allow_headers').with_value('HeaderThree,HeaderFour')
+      end
+    end
+
+    context 'with parameters in array' do
+      let (:params) do
+        {
+          :allowed_origin    => ['https://horizon.example.com', 'https://horizon.example2.com'],
+          :expose_headers    => ['HeaderOne', 'HeaderTwo'],
+          :allow_methods     => ['GET', 'HEAD'],
+          :allow_headers     => ['HeaderThree', 'HeaderFour'],
+        }
+      end
+
+      it 'configure cors params' do
+       is_expected.to contain_keystone_config('cors/allowed_origin').with_value('https://horizon.example.com,https://horizon.example2.com')
+       is_expected.to contain_keystone_config('cors/expose_headers').with_value('HeaderOne,HeaderTwo')
+       is_expected.to contain_keystone_config('cors/allow_methods').with_value('GET,HEAD')
+       is_expected.to contain_keystone_config('cors/allow_headers').with_value('HeaderThree,HeaderFour')
+      end
     end
   end
 
