97ee84e76f28289eca5884d151d85773a4052d44
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-01-14 21:57:14 +0900

    Add support for [healthcheck] allowed_source_ranges
    Change-Id: Iddb38da980cf01536185c6c910ca68ac35c8a436

--- manifests/healthcheck.pp
+++ manifests/healthcheck.pp
@@ -13,6 +13,11 @@
 #   that information back as part of a request.
 #   Defaults to $facts['os_service_default']
 #
+# [*allowed_source_ranges*]
+#   (Optional) A list of network addresses to limit source ip allowed to access
+#   healthcheck information.
+#   Defaults to $facts['os_service_default']
+#
 # [*disable_by_file_path*]
 #   (Optional) Check the presence of a file to determine if an application
 #   is running on a port.
@@ -26,16 +31,19 @@
 define oslo::healthcheck(
   $detailed              = $facts['os_service_default'],
   $backends              = $facts['os_service_default'],
+  $allowed_source_ranges = $facts['os_service_default'],
   $disable_by_file_path  = $facts['os_service_default'],
   $disable_by_file_paths = $facts['os_service_default'],
 ) {
 
   $backends_real = join(any2array($backends), ',')
+  $allowed_source_ranges_real = join(any2array($allowed_source_ranges), ',')
   $disable_by_file_paths_real = join(any2array($disable_by_file_paths), ',')
 
   $healthcheck_options = {
     'healthcheck/detailed'              => { value => $detailed },
     'healthcheck/backends'              => { value => $backends_real },
+    'healthcheck/allowed_source_ranges' => { value => $allowed_source_ranges_real },
     'healthcheck/disable_by_file_path'  => { value => $disable_by_file_path },
     'healthcheck/disable_by_file_paths' => { value => $disable_by_file_paths_real},
   }
