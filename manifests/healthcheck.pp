c92a469863ec9b77652dcbc5bfef00a3c1de98a9
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-07-08 22:18:53 +0900

    Add support for [healthcheck] enable_by_file_paths
    Depends-on: https://review.opendev.org/920055
    Change-Id: Ifba5f5b7346a186031df88f6fc34a8693aa54a94

--- manifests/healthcheck.pp
+++ manifests/healthcheck.pp
@@ -32,6 +32,11 @@
 #   is running on a port. Expects a "port:path" list of strings.
 #   Defaults to $facts['os_service_default']
 #
+# [*enable_by_file_paths*]
+#   (Optional) Check the presence of files. Used by EnableByFilesHealthcheck
+#   plugin.
+#   Defaults to $facts['os_service_default']
+#
 define oslo::healthcheck(
   $detailed                = $facts['os_service_default'],
   $backends                = $facts['os_service_default'],
@@ -39,11 +44,13 @@ define oslo::healthcheck(
   $ignore_proxied_requests = $facts['os_service_default'],
   $disable_by_file_path    = $facts['os_service_default'],
   $disable_by_file_paths   = $facts['os_service_default'],
+  $enable_by_file_paths    = $facts['os_service_default'],
 ) {
 
   $backends_real = join(any2array($backends), ',')
   $allowed_source_ranges_real = join(any2array($allowed_source_ranges), ',')
   $disable_by_file_paths_real = join(any2array($disable_by_file_paths), ',')
+  $enable_by_file_paths_real = join(any2array($enable_by_file_paths), ',')
 
   $healthcheck_options = {
     'healthcheck/detailed'                => { value => $detailed },
@@ -52,6 +59,7 @@ define oslo::healthcheck(
     'healthcheck/ignore_proxied_requests' => { value => $ignore_proxied_requests },
     'healthcheck/disable_by_file_path'    => { value => $disable_by_file_path },
     'healthcheck/disable_by_file_paths'   => { value => $disable_by_file_paths_real },
+    'healthcheck/enable_by_file_paths'    => { value => $enable_by_file_paths_real },
   }
   create_resources($name, $healthcheck_options)
 }
